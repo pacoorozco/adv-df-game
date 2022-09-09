729e47dd6bf495429dbbdd94dd9173ec83a8f696
pacoorozco <paco@pacoorozco.info>
2022-09-09 09:34:42 +0200

    Schedule question publication every 15 minutes
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -48,7 +48,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(PublishScheduledQuestions::class)->hourly();
+        $schedule->command(PublishScheduledQuestions::class)->->everyFifteenMinutes();
     }
 
     /**
