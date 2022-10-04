1aca818ba1fb2129a4b49391e6c779ff11bd2c37
pacoorozco <paco@pacoorozco.info>
2022-10-04 11:54:00 +0200

    Improve the update command
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- utils/update-gamify-laravel.sh
+++ utils/update-gamify-laravel.sh
@@ -2,21 +2,107 @@
 #
 # sudo apt install jq wget curl
 # usage: github-install user/repo
-set -e
 
 REPO=pacoorozco/gamify-laravel
 
-mkdir -p /tmp/gh-install
-URL="https://api.github.com/repos/${REPO}/releases/latest"
-echo "Reading... ${URL}"
+##########################################################################
+# DO NOT MODIFY BEYOND THIS LINE
+##########################################################################
+# Script exits immediately if any command within it exits with a non-zero status
+set -o errexit
+# Script will catch the exit status of a previous command in a pipe.
+set -o pipefail
+# Script exits immediately if tries to use an undeclared variables.
+set -o nounset
+# Uncomment this to enable debug
+# set -o xtrace
 
-FILENAME=$(curl -s "${URL}" | jq -r '.assets[].name')
-[ -e "/tmp/gh-install/${FILENAME}" ] && rm "/tmp/gh-install/${FILENAME}"
-echo "Downloading... ${FILENAME}"
+TEMP_DIR=/tmp/gh-install
 
-curl -s "${URL}" | jq -r --arg filename "${FILENAME}" '.assets[] | select(.name == $filename) | .browser_download_url' | wget -i- -q --show-progress -P /tmp/gh-install
+readonly REPO
+readonly TEMP_DIR
 
-unzip -q -o "/tmp/gh-install/${FILENAME}" -x "tests/*" "storage/*" "README.md" "CONTRIBUTE.md" "CODE_OF_CONDUCT.md" "dist/*"
-[ -e "${FILENAME}" ] && rm "${FILENAME}"
+update_from_tag() {
+    local tag_name=$1
 
+    local url="https://api.github.com/repos/${REPO}/releases/${tag_name}"
+    echo "Reading... ${url}"
 
+    local filename
+    filename=$(curl -s "${url}" | jq -r '.assets[].name')
+
+    [ -e "/tmp/gh-install/${filename}" ] && rm "${TEMP_DIR}/${filename}"
+    echo "Downloading... ${filename}"
+
+    curl --silent "${URL}" | jq -r --arg filename "${filename}" '.assets[] | select(.name == $filename) | .browser_download_url' | wget -i- -q --show-progress -P ${TEMP_DIR}
+
+    unzip -q -o "${TEMP_DIR}/${filename}" -x "tests/*" "storage/*" "README.md" "CONTRIBUTE.md" "CODE_OF_CONDUCT.md" "dist/*"
+    [ -e "${filename}" ] && rm "${filename}"
+}
+
+update_from_branch() {
+    local branch_name=$1
+
+    local url="http://github.com/${REPO}/archive/${branch_name}.zip"
+
+    local filename=${branch_name}.zip
+    [ -e "${TEMP_DIR}/${filename}" ] && rm "${TEMP_DIR}/${filename}"
+    echo "Downloading... ${filename}"
+
+    curl --silent --location "${url}" --output "${TEMP_DIR}/${filename}"
+
+    update_files "${TEMP_DIR}/${filename}"
+    #[ -e "${filename}" ] && rm "${filename}"
+
+}
+
+update_files() {
+    local -r zip_file=$1
+
+    unzip -q -o "${zip_file}"
+
+    sources_to_copy=(app config lang server.php artisan composer.json resources bootstrap composer.lock database public routes)
+
+    for source in "${sources_to_copy[@]}"; do
+        echo "Updating ${source}..."
+        cp -apr "${source}" .
+    done
+}
+
+main() {
+
+    mkdir -p ${TEMP_DIR}
+
+    while [ $# -gt 0 ]; do
+
+        case $1 in
+        -t | --tagged)
+            update_from_tag latest
+            exit
+            ;;
+        -b | --branch)
+            update_from_branch main
+            exit
+            ;;
+        --) # End of all options.
+            shift
+            break
+            ;;
+        -?*)
+            echo "Unknown option (ignored): $1"
+            ;;
+        *) # Default case: No more options, so break out of the loop.
+            break
+            ;;
+        esac
+
+        shift
+
+    done
+}
+
+##########################################################################
+# Main code
+##########################################################################
+
+main "${@}"
